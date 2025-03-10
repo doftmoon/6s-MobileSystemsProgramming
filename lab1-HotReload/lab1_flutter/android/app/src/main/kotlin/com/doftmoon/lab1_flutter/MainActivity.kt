291180b4e3b739eb914399dbc607de2c85cd7288
Artyom <86550931+doftmoon@users.noreply.github.com>
2025-03-10 09:23:16 +0300

    feat: add lab1, lab2

---
+++ lab1-HotReload/lab1_flutter/android/app/src/main/kotlin/com/doftmoon/lab1_flutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.doftmoon.lab1_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lab2_dart_base/android/app/src/main/kotlin/com/doftmoon/lab2_dart_base/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.doftmoon.lab2_dart_base
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
